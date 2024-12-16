99f1683073939bc1a31928995151d099b32123a8
Olabisim <ajoseholabisi@gmail.com>
2024-12-16 15:47:51 +0000

    allowed hosts configurations

--- gravitaionalforceits/settings.py
+++ gravitaionalforceits/settings.py
@@ -26,11 +26,10 @@ SECRET_KEY = 'django-insecure-5zml+yehfb%@4py7!4c9s)g)g_ip5uo!9ex+kx9t+m)6h)r+8a
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "gravitational-force-its.onrender.com",
+    "ratio-its.onrender.com",
     "localhost",              
     "127.0.0.1"               
 ]
-
 
 # Application definition
 
